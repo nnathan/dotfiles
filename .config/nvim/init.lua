24f50bca6a6d8f84cb21cbfe901f68b828b40b9f
Naveen Nathan <naveen@lastninja.net>
2026-01-09 11:21:56 +1100

    nvim: remove a treesitter config line that was breaking newer neovim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -940,7 +940,6 @@ require("lazy").setup({
 	{ -- Highlight, edit, and navigate code
 		"nvim-treesitter/nvim-treesitter",
 		build = ":TSUpdate",
-		main = "nvim-treesitter.configs", -- Sets main module to use for opts
 		-- [[ Configure Treesitter ]] See `:help nvim-treesitter`
 		opts = {
 			ensure_installed = {
