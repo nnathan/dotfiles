b7bf34becab4068ee110fea21eeca82ca3a4b0b2
Naveen Nathan <naveen@lastninja.net>
2026-03-17 03:26:26 +1100

    nvim: treesitter appeared to be broken

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -952,13 +952,16 @@ require("lazy").setup({
 	-- {{{ [[ Treesitter ]]
 	{ -- Highlight, edit, and navigate code
 		"nvim-treesitter/nvim-treesitter",
+		lazy = false,
 		build = ":TSUpdate",
+		branch = "main",
 		-- [[ Configure Treesitter ]] See `:help nvim-treesitter`
-		opts = {
-			ensure_installed = {
+		config = function()
+			local parsers = {
 				"bash",
 				"c",
 				"cpp",
+				"c_sharp",
 				"diff",
 				"go",
 				"html",
@@ -974,26 +977,46 @@ require("lazy").setup({
 				"typescript",
 				"vim",
 				"vimdoc",
-			},
+			}
 
-			-- Autoinstall languages that are not installed
-			auto_install = true,
-			highlight = {
+			require("nvim-treesitter").install(parsers)
+			vim.api.nvim_create_autocmd("FileType", {
+				callback = function(args)
+					local buf, filetype = args.buf, args.match
+
+					local language = vim.treesitter.language.get_lang(filetype)
+					if not language then
+						return
+					end
+
+					-- check if parser exists and load it
+					if not vim.treesitter.language.add(language) then
+						return
+					end
+					-- enables syntax highlighting and other treesitter features
+					vim.treesitter.start(buf, language)
+
+					-- enables treesitter based folds
+					-- for more info on folds see `:help folds`
+					-- vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+					-- vim.wo.foldmethod = 'expr'
+
+					-- enables treesitter based indentation
+					vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+				end,
+			})
+
+			-- unsure if these local definitions do anything
+			local highlight = {
 				enable = true,
 				-- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
 				--  If you are experiencing weird indenting issues, add the language to
 				--  the list of additional_vim_regex_highlighting and disabled languages for indent.
 				--additional_vim_regex_highlighting = { "ruby" },
 				additional_vim_regex_highlighting = true,
-			},
-			indent = { enable = true, disable = { "python" } },
-		},
-		-- There are additional nvim-treesitter modules that you can use to interact
-		-- with nvim-treesitter. You should go explore a few and see what interests you:
-		--
-		--    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
-		--    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
-		--    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
+			}
+			local indent = { enable = true, disable = { "python" } }
+		end,
 	},
 	-- }}}
 
